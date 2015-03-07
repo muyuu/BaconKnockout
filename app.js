79d62abb5f65731431864bdff4316aed98beba4e
muyuu <anticyborg@gmail.com>
2015-03-07 14:52:45 +0900

    add editorconfig

--- app.js
+++ app.js
@@ -1,74 +1,77 @@
 var app = app || {};
 
 (function(w, $, _, ko, app){
-  // -------------------------------
-  // data binding
-  // -------------------------------
-  function ViewModel (){
-    // repositories binding
-    this.repos = ko.observable();
-  }
+    // -------------------------------
+    // data binding
+    // -------------------------------
+    function ViewModel(){
+        // repositories binding
+        this.repos = ko.observable();
+    }
 
-  var viewModel = new ViewModel()
-  ko.applyBindings(viewModel);
+    var viewModel = new ViewModel()
+    ko.applyBindings(viewModel);
 
 
+    // -------------------------------
+    // stream
+    // -------------------------------
 
-  // -------------------------------
-  // stream
-  // -------------------------------
+    // make input stream
+    var inputStream = $('#query').asEventStream('input')
+        .map(function(e){
+            return $(e.target).val();
+        });
 
-  // make input stream
-  var inputStream = $('#query').asEventStream('input')
-    .map(function(e){
-      return $(e.target).val();
+    // make query stream
+    var queryStream = inputStream
+        .throttle(500)
+        .filter(function(text){
+            return text.length > 0;
+        })
+        .map(function(text){
+            return 'https://api.github.com/search/repositories?q=' + text;
+        });
+
+    // make repositories stream
+    var repositoriesStream = queryStream
+        .flatMap(function(query){
+            return Bacon.fromPromise($.ajax({ url: query }));
+        })
+        .map(function(res){
+            return res.items;
+        });
+
+    // sort stream
+    var ascSortStream = $("#ascBtn").asEventStream('click')
+        .map(function(){
+            var source = viewModel.repos() || [];
+            return _.sortBy(source, function(repo){
+                return repo.name;
+            });
+        });
+
+    var descSortStream = $("#descBtn").asEventStream('click')
+        .map(function(){
+            var source = viewModel.repos() || [];
+            return _.sortBy(source, function(repo){
+                return repo.name;
+            }).reverse();
+        });
+
+    // merge sort stream
+    var sortStream = ascSortStream.merge(descSortStream);
+
+
+    // render
+    repositoriesStream.onValue(function(repos){
+        // knockout binding
+        viewModel.repos(repos);
     });
 
-  // make query stream
-  var queryStream = inputStream
-    .throttle(500)
-    .filter(function(text){
-      return text.length > 0;
-    })
-    .map(function(text){
-      return 'https://api.github.com/search/repositories?q=' + text;
+    // render sort
+    sortStream.onValue(function(repos){
+        viewModel.repos(repos);
     });
-
-  // make repositories stream
-  var repositoriesStream = queryStream
-    .flatMap(function(query){
-      return Bacon.fromPromise($.ajax({url: query}));
-    })
-    .map(function(res){
-      return res.items;
-    });
-
-  // sort stream
-  var ascSortStream = $("#ascBtn").asEventStream('click')
-    .map(function(){
-      var source = viewModel.repos() || [];
-      return _.sortBy(source, function(repo){ return repo.name; });
-    });
-
-  var descSortStream = $("#descBtn").asEventStream('click')
-    .map(function(){
-      var source = viewModel.repos() || [];
-      return _.sortBy(source, function(repo){ return repo.name; }).reverse();
-    });
-
-  // merge sort stream
-  var sortStream = ascSortStream.merge(descSortStream);
-
-
-  // render
-  repositoriesStream.onValue(function(repos){
-    // knockout binding
-    viewModel.repos(repos);
-  });
-
-  // render sort
-  sortStream.onValue(function(repos){
-    viewModel.repos(repos);
-  });
 })(window, jQuery, _, ko, app);
 
